f4490fbe8f5e9365c883ec5f2ce02069a40b2ec8
Ernest <ernestwinata@gmail.com>
2023-07-13 08:07:26 -0700

    create initial commit

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,21 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    return [badge_maker(name) for name in names]
 
 def assign_rooms(names):
-    return None
+    rooms = range(1, 9)
+    assignments = []
+    for room in rooms:
+        assignments.append(f'Hello, {names[room - 1]}! You\'ll be assigned to room {room}!')
+    
+    return assignments
 
 def printer(names):
-    return None
+    badges = batch_badge_creator(names)
+    room_assignments = assign_rooms(names)
+    for badge in badges:
+        print(badge)
+    for assignment in room_assignments:
+        print(assignment)
